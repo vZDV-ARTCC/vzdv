8b5a3d945713e916fde0df571d31cacdb6a1a3e6
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2026-06-19 16:35:59 -0600

    refactor config shape, add KAPA, some tests

--- vzdv/src/ids.rs
+++ vzdv/src/ids.rs
@@ -17,41 +17,43 @@ type CloudLayerAGL = u16;
 pub struct AirportProcedure {
     pub try_match: Option<TryMatchProcedure>,
     pub rules: Vec<FlowRule>,
+    pub flows: HashMap<String, Flow>,
 }
 
 impl AirportProcedure {
-    pub fn determine_flow(
-        &self,
-        weather: &AirportWeather,
-        atis_list: Vec<&Atis>,
-    ) -> Result<&FlowRule> {
+    pub fn determine_flow(&self, weather: &AirportWeather, atis_list: &[Atis]) -> Result<&Flow> {
+        let icao = format!("K{}", &weather.name);
+        // If there's an ATIS up for this airport, just use that
+        if let Some(atis) = atis_list.iter().find(|a| a.facility == icao) {
+            return self.flows.get(&atis.preset).ok_or_else(|| {
+                anyhow::anyhow!("Flow {} not found for airport {}", atis.preset, icao,)
+            });
+        }
+
         // If we have a procedure to try to "match" another airport like KAPA -> KDEN,
         // then see if they have an `Atis` up,
         // then find the corresponding flow
         // TODO: Determine based on IMC/VMC as well
-        if let Some(match_proc) = &self.try_match {
-            let match_icao = &match_proc.icao;
-
-            if let Some(matched_preset) = atis_list
-                .iter()
-                .find(|a| a.facility == match_proc.icao)
-                .map(|flow| &flow.preset)
-            {
-                if let Some(selected_preset) = match_proc.match_flows.get(matched_preset) {
-                    return self
-                        .rules
-                        .iter()
-                        .find(|r| &r.flow_name == selected_preset)
-                        .ok_or_else(|| anyhow!("Flow {} for {} was matched from {}, but is not present in rules config!", selected_preset, weather.name, match_icao));
-                } else {
-                    bail!(
-                        "Could not find matching flow for {}. Checked {}",
-                        weather.name,
-                        matched_preset
-                    );
-                }
-            }
-        }
+        // if let Some(match_proc) = &self.try_match {
+        //     let match_icao = &match_proc.icao;
+        //     if let Some(matched_preset) = atis_list
+        //         .iter()
+        //         .find(|a| a.facility == match_proc.icao)
+        //         .map(|flow| &flow.preset)
+        //     {
+        //         if let Some(selected_preset) = match_proc.match_flows.get(matched_preset) {
+        //             return self
+        //                 .flows.get(selected_preset)
+        //                 .ok_or_else(|| anyhow!("Flow {} for {} was matched from {}, but is not present in rules config!", selected_preset, icao, match_icao));
+        //         } else {
+        //             bail!(
+        //                 "Could not find matching flow for {}. Checked {}",
+        //                 icao,
+        //                 matched_preset
+        //             );
+        //         }
+        //     }
+        // }
 
         let wind_kts = if weather.wind.2 > 0 {
             weather.wind.2
@@ -59,24 +61,55 @@ impl AirportProcedure {
             weather.wind.1
         };
         let is_calm = wind_kts <= 1;
-        self.rules
+        let rule = self
+            .rules
             .iter()
             .find(|rule| {
-                ((rule.calm && is_calm)
-                    || rule
-                        .bounds
-                        .as_ref()
-                        .is_some_and(|r| r.is_wind_within_bounds(weather.wind.0)))
-                    && rule.conds.contains(&weather.conditions)
+                let is_and_matches_calm = is_calm && rule.calm;
+                let within_directional_bounds = rule
+                    .direction_bounds
+                    .as_ref()
+                    .is_some_and(|r| r.is_within_bounds(weather.wind.0))
+                    || rule.direction_bounds.is_none();
+                let within_speed_bounds = rule
+                    .speed_bounds
+                    .as_ref()
+                    .is_some_and(|r| r.is_within_bounds(wind_kts))
+                    || rule.speed_bounds.is_none();
+                let matches_conditions = rule.conds.contains(&weather.conditions);
+
+                is_and_matches_calm
+                    || (!rule.calm
+                        && within_directional_bounds
+                        && within_speed_bounds
+                        && matches_conditions)
             })
-            .ok_or_else(|| anyhow::anyhow!("No matching procedure rule found"))
+            .ok_or_else(|| anyhow::anyhow!("No matching procedure rule found"))?;
+
+        self.flows.get(&rule.use_flow).ok_or_else(|| {
+            anyhow::anyhow!(
+                "Flow {} not found for rule at {} {:?}",
+                rule.use_flow,
+                icao,
+                rule
+            )
+        })
     }
 }
 
 #[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct TryMatchProcedure {
     pub icao: String,
     pub match_flows: HashMap<String, String>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Flow {
+    pub name: String,
+    pub dep_rwys: Vec<String>,
+    pub arr_rwys: Vec<String>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -85,22 +118,34 @@ pub struct FlowRule {
     #[serde(default)]
     pub calm: bool,
     pub conds: Vec<WeatherConditions>,
-    pub flow_name: String,
-    pub bounds: Option<WindBounds>,
-    pub dep_rwys: Vec<String>,
-    pub arr_rwys: Vec<String>,
+    pub use_flow: String,
+    pub direction_bounds: Option<WindDirectionBounds>,
+    pub speed_bounds: Option<WindSpeedBounds>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct WindBounds {
+pub struct WindSpeedBounds {
+    min_kts: WindSpeed,
+    max_kts: WindSpeed,
+}
+
+impl WindSpeedBounds {
+    pub fn is_within_bounds(&self, wind_kts: WindSpeed) -> bool {
+        wind_kts >= self.min_kts && wind_kts <= self.max_kts
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct WindDirectionBounds {
     pub wind_from: WindDirection,
     pub clock_dir: ClockDir,
     pub wind_to: WindDirection,
 }
 
-impl WindBounds {
-    pub fn is_wind_within_bounds(&self, wind_dir: WindDirection) -> bool {
+impl WindDirectionBounds {
+    pub fn is_within_bounds(&self, wind_dir: WindDirection) -> bool {
         // If clock_dir is CW, then consider range (wind_from..=wind_to)
         // If clock_dir is CCW, then consider range (wind_to..=wind_from)
         // Adding 360 if we know if the wind dir wraps around 360
@@ -137,4 +182,95 @@ pub enum ClockDir {
     Clockwise,
     #[serde(rename = "ccw")]
     CounterClockwise,
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::Utc;
+
+    use crate::config::ConfigIDS;
+    use std::fs;
+
+    use super::*;
+
+    fn load_config() -> ConfigIDS {
+        let file_s = fs::read_to_string("../ids.json").unwrap();
+        serde_json::from_str(&file_s).unwrap()
+    }
+
+    /// Even if winds favor another flow, choose whichever vATIS has sent
+    #[test]
+    fn atis_override() {
+        let config = load_config();
+        let procedure = config.0.get("KAPA").unwrap();
+        let weather = AirportWeather {
+            ceiling: 0,
+            conditions: WeatherConditions::VFR,
+            name: "APA".into(),
+            raw: "".into(),
+            visibility: 10,
+            wind: (180, 5, 10),
+        };
+        let atis = Atis {
+            airport_conditions: "".into(),
+            atis_letter: "A".into(),
+            atis_type: "".into(),
+            facility: "KAPA".into(),
+            id: 0,
+            notams: "".into(),
+            preset: "NORTH VMC".into(),
+            timestamp: Utc::now(),
+            version: "".into(),
+        };
+
+        let flow = procedure.determine_flow(&weather, &[atis]).unwrap();
+        assert_eq!(flow.name, "NORTH VMC")
+    }
+
+    #[test]
+    fn flow_from_calm_winds() {
+        let config = load_config();
+        let procedure = config.0.get("KAPA").unwrap();
+        let weather = AirportWeather {
+            ceiling: 0,
+            conditions: WeatherConditions::VFR,
+            name: "APA".into(),
+            raw: "".into(),
+            visibility: 10,
+            wind: (350, 1, 0),
+        };
+
+        let flow = procedure.determine_flow(&weather, &[]).unwrap();
+        assert_eq!(flow.name, "SOUTH VMC")
+    }
+
+    #[test]
+    fn flow_from_non_calm_winds() {
+        let config = load_config();
+        let procedure = config.0.get("KAPA").unwrap();
+        let weather = AirportWeather {
+            ceiling: 0,
+            conditions: WeatherConditions::VFR,
+            name: "APA".into(),
+            raw: "".into(),
+            visibility: 10,
+            wind: (350, 5, 10),
+        };
+
+        let flow = procedure.determine_flow(&weather, &[]).unwrap();
+        assert_eq!(flow.name, "NORTH VMC")
+    }
+
+    #[test]
+    fn dir_bounds_cw_north_wrap() {
+        let bounds = WindDirectionBounds {
+            wind_from: 260,
+            clock_dir: ClockDir::Clockwise,
+            wind_to: 79,
+        };
+
+        assert!(bounds.is_within_bounds(350));
+        assert!(bounds.is_within_bounds(0));
+        assert!(!bounds.is_within_bounds(180));
+    }
 }
